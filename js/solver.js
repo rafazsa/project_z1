069d6468af8a77bec354c0a11de7ab2725f3f28f
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2020-05-04 14:47:49 -0400

    Added - Função Ca/Mg

--- js/solver.js
+++ js/solver.js
@@ -1,7 +1,10 @@
 const caoA = 0.3923;
-const mgA = 0.3577;
+const mgoA = 0.3577;
 const caoB = 0.6863;
 const mgoB = 0.0637;
+
+const funcao_ca = 1.7867992;
+const funcao_mg = 2.4832748;
 
 const calcario = {
   A: {
@@ -22,38 +25,55 @@ const calcario = {
   },
 };
 
-const calcA_ca = document.querySelector('input[name="ca_a"]');
-const calcA_mg = document.querySelector('input[name="mg_a"]');
-const calcA_prnt = document.querySelector('input[name="prnt_a"]');
-const calcA_acrescimoCa = document.querySelector(
+const input_funcao_ca = document.querySelector('input[name="funcao_ca"]');
+const input_funcao_mg = document.querySelector('input[name="funcao_mg"]');
+
+const input_cao_a = document.querySelector("input[name='cao_a']");
+const input_cao_b = document.querySelector("input[name='cao_b']");
+const input_mgo_a = document.querySelector("input[name='mgo_a']");
+const input_mgo_b = document.querySelector("input[name='mgo_b']");
+
+const input_ca_a = document.querySelector('input[name="ca_a"]');
+const input_mg_a = document.querySelector('input[name="mg_a"]');
+const input_prnt_a = document.querySelector('input[name="prnt_a"]');
+const input_acrescimoCa_a = document.querySelector(
   'input[name="acrescimo_ca_a"]'
 );
-const calcA_acrescimoMg = document.querySelector(
+const input_acrescimoMg_a = document.querySelector(
   'input[name="acrescimo_mg_a"]'
 );
-const calcA_pn = document.querySelector('input[name="acrescimo_pn_a"]');
+const input_pn_a = document.querySelector('input[name="acrescimo_pn_a"]');
 
-const calcB_ca = document.querySelector('input[name="ca_b"]');
-const calcB_mg = document.querySelector('input[name="mg_b"]');
-const calcB_prnt = document.querySelector('input[name="prnt_b"]');
-const calcB_acrescimoCa = document.querySelector(
+const input_ca_b = document.querySelector('input[name="ca_b"]');
+const input_mg_b = document.querySelector('input[name="mg_b"]');
+const input_prnt_b = document.querySelector('input[name="prnt_b"]');
+const input_acrescimoCa_b = document.querySelector(
   'input[name="acrescimo_ca_b"]'
 );
-const calcB_acrescimoMg = document.querySelector(
+const input_acrescimoMg_b = document.querySelector(
   'input[name="acrescimo_mg_b"]'
 );
-const calcB_pn = document.querySelector('input[name="acrescimo_pn_b"]');
+const input_pn_b = document.querySelector('input[name="acrescimo_pn_b"]');
 
-calcA_ca.value = calcario.A.Ca;
-calcA_mg.value = calcario.A.Mg;
-calcA_prnt.value = calcario.A.PRNT;
-calcA_acrescimoCa.value = calcario.A.acrescimoCa;
-calcA_acrescimoMg.value = calcario.A.acrescimoMg;
-calcA_pn.value = calcario.A.PN;
+input_funcao_ca.value = funcao_ca;
+input_funcao_mg.value = funcao_mg;
 
-calcB_ca.value = calcario.B.Ca;
-calcB_mg.value = calcario.B.Mg;
-calcB_prnt.value = calcario.B.PRNT;
-calcB_acrescimoCa.value = calcario.B.acrescimoCa;
-calcB_acrescimoMg.value = calcario.B.acrescimoMg;
-calcB_pn.value = calcario.B.PN;
+input_cao_a.value = caoA;
+input_cao_b.value = caoB;
+
+input_mgo_a.value = mgoA;
+input_mgo_b.value = mgoB;
+
+input_ca_a.value = calcario.A.Ca;
+input_mg_a.value = calcario.A.Mg;
+input_prnt_a.value = calcario.A.PRNT;
+input_acrescimoCa_a.value = calcario.A.acrescimoCa;
+input_acrescimoMg_a.value = calcario.A.acrescimoMg;
+input_pn_a.value = calcario.A.PN;
+
+input_ca_b.value = calcario.B.Ca;
+input_mg_b.value = calcario.B.Mg;
+input_prnt_b.value = calcario.B.PRNT;
+input_acrescimoCa_b.value = calcario.B.acrescimoCa;
+input_acrescimoMg_b.value = calcario.B.acrescimoMg;
+input_pn_b.value = calcario.B.PN;
